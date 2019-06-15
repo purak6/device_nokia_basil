77894fbfd8fd93733f30124f457afc85e9b5c880
Ram9199 <sriramscience7@gmail.com>
2019-06-15 08:55:27 +0530

    Basil: Init
    Imported Stuff from Nokia 7.1 PLUS
    
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+ Nokia 7 plus (codenamed _"BASIL"_) are high-end mid-range smartphones from Nokia.
+
+Nokia 7 Plus was announced and released in march 2018.
+
+## Device specifications
+
+| Device       |   Nokia 7 plus                                  |
+| -----------: | :---------------------------------------------- |
+| SoC          | Qualcomm SDM660 Snapdragon 660                  |
+| CPU          | 8x Qualcomm® Kryo™ 260 up to 1.8GHz             |
+| GPU          | Adreno 512                                      |
+| Memory       | 4GB                                             |
+| Shipped Android version | 8.1.0                                |
+| Storage      | 64GB eMMC 5.1 flash storage                     |
+| Battery      | Non-removable Li-Po 3800 mAh                    |
+
+
+
---
+++ lluvia_basil.mk
@@ -0,0 +1,77 @@
+# 
+# Copyright (C) 2018-2019 LLuvia Open Source
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Inherit from BASIL device
+$(call inherit-product, device/nokia/basil/device.mk)
+$(call inherit-product, vendor/lluvia/config/common.mk)
+
+# A/B updater
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+# The following modules are included in debuggable builds only.
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl \
+    update_engine_client
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    bootctrl.sdm660
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.sdm660 \
+    libcutils \
+    libgptutils \
+    libz
+
+# Device identifiers
+PRODUCT_NAME := lluvia_basil
+PRODUCT_BRAND := nokia
+PRODUCT_DEVICE := basil
+PRODUCT_MANUFACTURER := nokia
+PRODUCT_MODEL := Nokia 7 Plus
+
+PRODUCT_GMS_CLIENTID_BASE := android-nokia
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="basil" \
+    PRODUCT_NAME="basil" \
+    PRIVATE_BUILD_DESC="basil-user 9.0.0 OPN1.121119.012 V8.5.12.0.OELSIFA release-keys"
+
+BUILD_FINGERPRINT := nokia/basil/basil:9.0.0/OPN1.121119.012/V8.5.12.0.OELSIFA:user/release-keys
+
+TARGET_VENDOR := nokia
